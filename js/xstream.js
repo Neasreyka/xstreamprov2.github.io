776c5323e8997ce3649681af7de92540a9a13a35
Fii <102344882+Neasreyka@users.noreply.github.com>
2023-11-27 15:31:27 +0530

    update

---
+++ js/xstream.js
@@ -0,0 +1,1 @@
+const xstream={async get(t,a,s){try{const r=await fetch(`${s.workers[Math.floor(Math.random()*s.workers.length)]}/api/${t}/${a}`),e=await r.json();return e.status?e:{status:!1}}catch{return{status:!1}}}};
